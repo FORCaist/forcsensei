7634b8ced1cea69ba5f1c99f398d2a0cab21e5d9
FORCaist <FORCaist.user@gmail.com>
2018-10-18 14:21:04 +1100

    initial package test

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forcsensei",
+    version="0.0.1",
+    author="FORCaist",
+    author_email="FORCaist.user@gmail.com",
+    description="A small example of forcsensei",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FORCaist",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
