d80d6cf7f6e42bd4d2c41c58f91239428dfdb671
FORCaist <FORCaist.user@gmail.com>
2018-10-19 15:24:30 +1100

    updated to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="forcsensei",
-    version="0.0.1",
+    version="0.0.2",
     author="FORCaist",
     author_email="FORCaist.user@gmail.com",
     description="A small example of forcsensei",
