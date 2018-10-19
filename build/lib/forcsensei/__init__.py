23ea5dfb4e893b3b990b15dea9c4f9610236589b
FORCaist <FORCaist.user@gmail.com>
2018-10-19 15:18:26 +1100

    updated package

---
+++ build/lib/forcsensei/__init__.py
@@ -0,0 +1,409 @@
+# FORCsensei module 
+# give command OPENBLAS_NUM_THREADS=1 in terminal if using multithreading
+
+import numpy as np
+import codecs as cd
+import scipy as sp
+from IPython.display import YouTubeVideo
+
+# define function which will look for lines in the header that start with certain strings
+def find_data_lines(fp):
+    """Helper function to identify measurement lines in a FORC data file.
+    
+    Given the various FORC file formats, measurements lines are considered to be those which:
+    Start with a '+' or,
+    Start with a '-' or,
+    Are blank (i.e. lines between FORCs and calibration points) or,
+    Contain a ','
+
+    Inputs:
+    fp: file identifier
+
+    Outputs:
+    line: string corresponding to data line that meets the above conditions
+    """
+    return [line for line in fp if ((line.startswith('+')) or (line.startswith('-')) or (line.strip()=='') or line.find(',')>-1.)]
+
+#function to parse calibration points and provide time stamps
+def lines_that_start_with(string, fp):
+    """Helper function to lines in a FORC data file that start with a given string
+    
+    Inputs:
+    string: string to compare lines to 
+    fp: file identifier
+
+    Outputs:
+    line: string corresponding to data line that meets the above conditions
+    """
+    return [line for line in fp if line.startswith(string)]
+
+def parse_header(file,string):
+    """Function to extract instrument settings from FORC data file header
+    
+    Inputs:
+    file: name of data file (string)    
+    string: instrument setting to be extracted (string)
+
+    Outputs:
+    output: value of instrument setting [-1 if setting doesn't exist] (float)
+    """
+    output=-1 #default output (-1 corresponds to no result, i.e. setting doesn't exist)
+    with cd.open(file,"r",encoding='latin9') as fp: #open the data file (latin9 encoding seems to work, UTF and ASCII don't)
+        for line in lines_that_start_with(string, fp): #find the line starting with the setting name
+            idx = line.find('=') #Some file formats may contain an '='
+            if idx>-1.: #if '=' found
+                output=float(line[idx+1:]) #value taken as everything to right of '='
+            else: # '=' not found
+                idx = len(string) #length of the setting string 
+                output=float(line[idx+1:])  #value taken as everything to right of the setting name 
+
+    return output
+
+def parse_units(file):
+    """Function to extract instrument unit settings ('') from FORC data file header
+    
+    Inputs:
+    file: name of data file (string)    
+
+    Outputs:
+    CGS [Cgs setting] or SI [Hybrid SI] (string)
+    """
+    string = 'Units of measure' #header definition of units
+    with cd.open(file,"r",encoding='latin9') as fp: #open the data file (latin9 encoding seems to work, UTF and ASCII don't)
+        for line in lines_that_start_with(string, fp): #find the line starting with the setting name
+            idxSI = line.find('Hybrid SI') #will return location if string is found, otherwise returns -1
+            idxCGS = line.find('Cgs') #will return location if string is found, otherwise returns -1
+    
+    if idxSI>idxCGS: #determine which unit string was found in the headerline and output
+        return 'SI'
+    else:
+        return 'CGS'
+
+def parse_mass(file):
+    """Function to extract instrument unit settings ('') from FORC data file header
+    
+    Inputs:
+    file: name of data file (string)    
+
+    Outputs:
+    Mass in g or N/A
+    """
+    string = 'Mass' #header definition of units
+    with cd.open(file,"r",encoding='latin9') as fp: #open the data file (latin9 encoding seems to work, UTF and ASCII don't)
+        for line in lines_that_start_with(string, fp): #find the line starting with the setting name
+            idx = line.find('=') #Some file formats may contain an '='
+            if idx>-1.: #if '=' found
+                output=(line[idx+1:]) #value taken as everything to right of '='
+            else: # '=' not found
+                idx = len(string) #length of the setting string 
+                output=(line[idx+1:])  #value taken as everything to right of the setting name
+
+    return output
+
+def calibration_times(file, Npts):
+    """Function to estimate the time at which calibration points were measured in a FORC sequence
+    
+    Follows the procedure given in:
+    R. Egli (2013) VARIFORC: An optimized protocol for calculating non-regular first-order reversal curve (FORC) diagrams. Global and Planetary Change, 110, 302-320, doi:10.1016/j.gloplacha.2013.08.003.
+
+    Inputs:
+    file: name of data file (string)    
+    Npts: number of calibration points (int)
+
+    Outputs:
+    tcal_k: Estimated times at which the calibration points were measured (float)
+    """    
+    Units=parse_units(file) #determine measurement system (CGS or SI)
+
+    string='PauseRvrsl' #Pause at reversal field (new file format, -1 if not available)
+    tr0=parse_header(file,string)
+    
+    string='PauseNtl' #Pause at reversal field (old file format, -1 if not available)
+    tr1=parse_header(file,string)
+
+    tr=np.max((tr0,tr1)) #select Pause value depending on file format
+    
+    string='Averaging time' #Measurement averaging time 
+    tau=parse_header(file,string)
+
+    string='PauseCal' #Pause at calibration point
+    tcal=parse_header(file,string)
+
+    string='PauseSat' #Pause at saturation field
+    ts=parse_header(file,string)
+
+    string='SlewRate' #Field slewrate
+    alpha=parse_header(file,string)
+
+    string='HSat' #Satuation field
+    Hs=parse_header(file,string)
+
+    string='Hb2' #upper Hb value for the FORC box
+    Hb2=parse_header(file,string)
+
+    string='Hb1' #lower Hb value for the FORC box
+    Hb1=parse_header(file,string)
+
+    string='Hc2' #upper Hc value for the FORC box (n.b. Hc1 is assumed to be 0)
+    Hc2=parse_header(file,string)
+
+    string='NForc' # Numer of measured FORCs (new file format, -1 if not available)
+    N0=parse_header(file,string)
+
+    string='NCrv'  # Numer of measured FORCs (old file format, -1 if not available)
+    N1=parse_header(file,string)
+
+    N=np.max((N0,N1)) #select Number of FORCs depending on file format
+
+    if Units=='CGS':
+        alpha=alpha/1E4 #convert from Oe to T
+        Hs=Hs/1E4 #convert from Oe to T
+        Hb2=Hb2/1E4 #convert from Oe to T
+        Hb1=Hb1/1E4 #convert from Oe to T
+    
+    dH = (Hc2-Hb1+Hb2)/N #estimated field spacing
+    
+    #now following Elgi's estimate of the measurement time
+    nc2 = Hc2/dH
+    Dt1 = tr + tau + tcal + ts + 2.*(Hs-Hb2-dH)/alpha
+    Dt2 = tr + tau + (Hc2-Hb2-dH)/alpha
+
+    Npts=int(Npts)
+    tcal_k=np.zeros(Npts)
+    
+    for k in range(1,Npts+1):
+        if k<=1+nc2:
+            tcal_k[k-1]=k*Dt1-Dt2+dH/alpha*k**2+(tau-dH/alpha)*(k-1)**2
+        else:
+            tcal_k[k-1]=k*Dt1-Dt2+dH/alpha*k**2+(tau-dH/alpha)*((k-1)*(1+nc2)-nc2)
+
+    return tcal_k
+
+def measurement_times(file,Fk,Fj):
+    """Function to estimate the time at which magnetization points were measured in a FORC sequence
+    
+    Follows the procedure given in:
+    R. Egli (2013) VARIFORC: An optimized protocol for calculating non-regular first-order reversal curve (FORC) diagrams. Global and Planetary Change, 110, 302-320, doi:10.1016/j.gloplacha.2013.08.003.
+
+    Inputs:
+    file: name of data file (string)    
+    Fk: FORC indicies (int)
+    Fj: Measurement indicies within given FORC
+
+    Outputs:
+    Ft: Estimated times at which the magnetization points were measured (float)
+    """    
+    Units=parse_units(file) #determine measurement system (CGS or SI)
+
+    string='PauseRvrsl' #Pause at reversal field (new file format, -1 if not available)
+    tr0=parse_header(file,string)
+    
+    string='PauseNtl' #Pause at reversal field (old file format, -1 if not available)
+    tr1=parse_header(file,string)
+
+    tr=np.max((tr0,tr1)) #select Pause value depending on file format
+    
+    string='Averaging time' #Measurement averaging time 
+    tau=parse_header(file,string)
+
+    string='PauseCal' #Pause at calibration point
+    tcal=parse_header(file,string)
+
+    string='PauseSat' #Pause at saturation field
+    ts=parse_header(file,string)
+
+    string='SlewRate' #Field slewrate
+    alpha=parse_header(file,string)
+
+    string='HSat' #Satuation field
+    Hs=parse_header(file,string)
+
+    string='Hb2' #upper Hb value for the FORC box
+    Hb2=parse_header(file,string)
+
+    string='Hb1' #lower Hb value for the FORC box
+    Hb1=parse_header(file,string)
+
+    string='Hc2' #upper Hc value for the FORC box (n.b. Hc1 is assumed to be 0)
+    Hc2=parse_header(file,string)
+
+    string='NForc' # Numer of measured FORCs (new file format, -1 if not available)
+    N0=parse_header(file,string)
+
+    string='NCrv'  # Numer of measured FORCs (old file format, -1 if not available)
+    N1=parse_header(file,string)
+
+    N=np.max((N0,N1)) #select Number of FORCs depending on file format
+
+    if Units=='CGS':
+        alpha=alpha/1E4 #convert from Oe to T
+        Hs=Hs/1E4 #convert from Oe to T
+        Hb2=Hb2/1E4 #convert from Oe to T
+        Hb1=Hb1/1E4 #convert from Oe to T
+
+    dH = (Hc2-Hb1+Hb2)/N #estimated field spacing
+    
+    #now following Elgi's estimate of the measurement time
+    nc2 = Hc2/dH
+
+    Dt1 = tr + tau + tcal + ts + 2.*(Hs-Hb2-dH)/alpha
+    Dt3 = Hb2/alpha
+
+    Npts=int(Fk.size)
+    Ft=np.zeros(Npts)
+    
+    for i in range(Npts):
+        if Fk[i]<=1+nc2:
+            Ft[i]=Fk[i]*Dt1+Dt3+Fj[i]*tau+dH/alpha*(Fk[i]*(Fk[i]-1))+(tau-dH/alpha)*(Fk[i]-1)**2
+        else:
+            Ft[i]=Fk[i]*Dt1+Dt3+Fj[i]*tau+dH/alpha*(Fk[i]*(Fk[i]-1))+(tau-dH/alpha)*((Fk[i]-1)*(1+nc2)-nc2)
+
+    return Ft
+
+def parse_calibration(file):
+    """Function to extract measured calibration points from a FORC sequence
+    
+    Inputs:
+    file: name of data file (string)    
+
+    Outputs:
+    Hcal: sequence of calibration fields [float, SI units]
+    Mcal: sequence of calibration magnetizations [float, SI units]
+    tcal: Estimated times at which the calibration points were measured (float, seconds)
+    """ 
+
+    dum=-9999.99 #dum value to indicate break in measurement seqence between FORCs and calibration points
+    N0=int(1E6) #assume that any file will have less than 1E6 measurements
+    H0=np.zeros(N0)*np.nan #initialize NaN array to contain field values
+    M0=np.zeros(N0)*np.nan #initialize NaN array to contain magnetization values
+    H0[0]=dum #first field entry is dummy value
+    M0[0]=dum #first magnetization entry is dummy value 
+
+    count=0 #counter to place values in arrays
+    with cd.open(file,"r",encoding='latin9') as fp: #open the data file (latin9 encoding seems to work, UTF and ASCII don't)
+        for line in find_data_lines(fp): #does the current line contain measurement data
+            count=count+1 #increase counter
+            idx = line.find(',') #no comma indicates a blank linw
+            if idx>-1: #line contains a comma
+                H0[count]=float(line[0:idx]) #assign field value (1st column)
+                line=line[idx+1:] #remove the leading part of the line (only characters after the first comma remain)
+                idx = line.find(',') #find next comman
+                if idx>-1: #comma found in line
+                    M0[count]=float(line[0:idx]) #read values up to next comma (assumes 2nd column is magnetizations)
+                else: #comma wasn't found   
+                    M0[count]=float(line) # magnetization value is just the remainder of the line 
+            else:
+                H0[count]=dum #line is blank, so fill with dummy value
+                M0[count]=dum #line is blank, so fill with dummy value
+
+    idx_start=np.argmax(H0!=dum) #find the first line that contains data            
+    M0=M0[idx_start-1:-1] #strip out leading dummy values from magnetizations, leaving 1 dummy at start of vector           
+    M0=M0[~np.isnan(M0)] #remove any NaNs at the end of the array
+    H0=H0[idx_start-1:-1] #strip out leading dummy values from magnetizations, leaving 1 dummy at start of vector
+    H0=H0[~np.isnan(H0)] #remove any NaNs at the end of the array
+
+    ## now need to pull out the calibration points, will be after alternate -9999.99 entries
+    idxSAT = np.array(np.where(np.isin(H0, dum))) #location of dummy values
+    idxSAT = np.ndarray.squeeze(idxSAT) #squeeze into 1D
+    idxSAT = idxSAT[0::2]+1 #every second index+1 should be calibration points
+
+    Hcal=H0[idxSAT[0:-1]] #calibration fields
+    Mcal=M0[idxSAT[0:-1]] #calibration magnetizations
+    tcal=calibration_times(file,Hcal.size) #estimate the time of each calibratio measurement
+
+    Units=parse_units(file)
+    if Units=='CGS': #ensure SI units
+        Hcal=Hcal/1E4 #convert from Oe to T
+        Mcal=Mcal/1E3 #convert from emu to Am^2
+
+    return Hcal, Mcal, tcal
+
+def parse_measurements(file):
+    """Function to extract measurement points from a FORC sequence
+    
+    Inputs:
+    file: name of data file (string)    
+
+    Outputs:
+    H: Measurement applied field [float, SI units]
+    Hr: Reversal field [float, SI units]
+    M: Measured magnetization [float, SI units]
+    Fk: Index of measured FORC (int)
+    Fj: Index of given measurement within a given FORC (int)
+    Ft: Estimated times at which the points were measured (float, seconds)
+    dH: Measurement field spacing [float SI units]
+    """ 
+
+    dum=-9999.99 #dum value to indicate break in measurement seqence between FORCs and calibration points
+    N0=int(1E6) #assume that any file will have less than 1E6 measurements
+    H0=np.zeros(N0)*np.nan #initialize NaN array to contain field values
+    M0=np.zeros(N0)*np.nan #initialize NaN array to contain magnetization values
+    H0[0]=dum #first field entry is dummy value
+    M0[0]=dum #first magnetization entry is dummy value 
+
+    count=0 #counter to place values in arrays
+    with cd.open(file,"r",encoding='latin9') as fp: #open the data file (latin9 encoding seems to work, UTF and ASCII don't)
+        for line in find_data_lines(fp): #does the current line contain measurement data
+            count=count+1 #increase counter
+            idx = line.find(',') #no comma indicates a blank linw
+            if idx>-1: #line contains a comma
+                H0[count]=float(line[0:idx]) #assign field value (1st column)
+                line=line[idx+1:] #remove the leading part of the line (only characters after the first comma remain)
+                idx = line.find(',') #find next comman
+                if idx>-1: #comma found in line
+                    M0[count]=float(line[0:idx]) #read values up to next comma (assumes 2nd column is magnetizations)
+                else: #comma wasn't found   
+                    M0[count]=float(line) # magnetization value is just the remainder of the line 
+            else:
+                H0[count]=dum #line is blank, so fill with dummy value
+                M0[count]=dum #line is blank, so fill with dummy value
+
+    idx_start=np.argmax(H0!=dum) #find the first line that contains data            
+    M0=M0[idx_start-1:-1] #strip out leading dummy values from magnetizations, leaving 1 dummy at start of vector           
+    M0=M0[~np.isnan(M0)] #remove any NaNs at the end of the array
+    H0=H0[idx_start-1:-1] #strip out leading dummy values from magnetizations, leaving 1 dummy at start of vector
+    H0=H0[~np.isnan(H0)] #remove any NaNs at the end of the array
+
+    ## determine indicies of each FORC
+    idxSAT = np.array(np.where(np.isin(H0, dum))) #find start address of each blank line
+    idxSAT = np.ndarray.squeeze(idxSAT) #squeeze into 1D
+    idxSTART = idxSAT[1::2]+1 #find start address of each FORC
+    idxEND = idxSAT[2::2]-1 ##find end address of each FORC
+
+    
+    #Extract first FORC to initialize arrays 
+    M=M0[idxSTART[0]:idxEND[0]+1] #Magnetization values
+    H=H0[idxSTART[0]:idxEND[0]+1] #Field values
+    Hr=np.ones(idxEND[0]+1-idxSTART[0])*H0[idxSTART[0]] #Reversal field values
+    Fk=np.ones(idxEND[0]+1-idxSTART[0]) #index number of FORC
+    Fj=np.arange(1,1+idxEND[0]+1-idxSTART[0])# measurement index within given FORC
+
+    #Extract remaining FORCs one by one into into a long-vector
+    for i in range(1,idxSTART.size):
+        M=np.concatenate((M,M0[idxSTART[i]:idxEND[i]+1]))
+        H=np.concatenate((H,H0[idxSTART[i]:idxEND[i]+1]))
+        Hr=np.concatenate((Hr,np.ones(idxEND[i]+1-idxSTART[i])*H0[idxSTART[i]]))
+        Fk=np.concatenate((Fk,np.ones(idxEND[i]+1-idxSTART[i])+i))
+        Fj=np.concatenate((Fj,np.arange(1,1+idxEND[i]+1-idxSTART[i])))
+    
+    Units=parse_units(file) #Ensure use of SI units
+    if Units=='CGS':
+        H=H/1E4 #Convert Oe into T
+        Hr=Hr/1E4 #Convert Oe into T
+        M=M/1E3 #Convert emu to Am^2
+
+    dH = np.mean(np.diff(H[Fk==np.max(Fk)])) #mean field spacing
+
+    Ft=measurement_times(file,Fk,Fj) #estimated time of each measurement point
+
+    return H, Hr, M, Fk, Fj, Ft, dH
+
+def play_tutorial(index):
+    
+    #define list of tutorial videos
+    tutorial = ['Z9DAztPG2lc'] #tutorial 1
+    tutorial.append('6kCS_nJC72g') #tutorial 2
+    tutorial.append('fZ0oB2wEBOI') #tutorial 3
+        
+    vid = YouTubeVideo(id = tutorial[index-1],autoplay=True)
+    display(vid)
